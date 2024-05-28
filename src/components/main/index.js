3e57d4ac46417af47b0137bd0829615c24f82b01
sahil <sahil@bisht.com>
2024-05-28 18:57:07 +0530

    fd

--- src/components/main/index.js
+++ src/components/main/index.js
@@ -7,7 +7,7 @@ export default function Main()
 {
     return <>
 
-    <BrowserRouter>
+    <BrowserRouter  basename={process.env.PUBLIC_URL}>
     <Routes>
        <Route path='' element={<Layout/>} >
             <Route index element={<Merge/>} />
